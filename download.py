cee56e4224e0e32b17d097fe7f6149f62ed8b859
James <p1n3tr335@gmail.com>
2018-09-06 11:53:55 -0400

    reinx-downloader

--- download.py
+++ download.py
@@ -2,28 +2,19 @@ import os, sys, zipfile
 
 cwd = os.getcwd()
 
+url = "https://dootnode.org/neko/builds/nightly/ReiNX-latest.zip"
+
 if(sys.version_info > (3, 0)):
     import urllib.request
-    urllib.request.urlretrieve("https://gist.githubusercontent.com/GarnetSunset/f8713eed6efd548fb4aded28c13b428f/raw", "latest.txt")
+    urllib.request.urlretrieve(url, "reinx.zip")
 else:
     import urllib2
-    with open("latest.txt",'wb') as f:
-        f.write(urllib2.urlopen("https://gist.githubusercontent.com/GarnetSunset/f8713eed6efd548fb4aded28c13b428f/raw").read())
-        f.close()
-
-with open("latest.txt", 'r') as myfile:
-    url=myfile.read().replace('\n', '')
-
-if(sys.version_info > (3, 0)):
-    urllib.request.urlretrieve(url, "chromeweb.zip")
-else:
-    with open("chromeweb.zip",'wb') as f:
+    with open("reinx.zip",'wb') as f:
         f.write(urllib2.urlopen(url).read())
         f.close()
 
-zipper = zipfile.ZipFile("chromeweb.zip", 'r')
+zipper = zipfile.ZipFile("reinx.zip", 'r')
 zipper.extractall(cwd)
 zipper.close()
 
-os.remove("chromeweb.zip")
-os.remove("latest.txt")
+os.remove("reinx.zip")
